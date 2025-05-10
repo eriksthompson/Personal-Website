0ad16ea88bf42cb1692640bf5d0ecd51110fc935
mintElekk <55054477+eriksthompson@users.noreply.github.com>
2025-05-10 17:12:13 -0500

    Update about.tsx

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -8,6 +8,7 @@ function ContactForm() {
       return <p>Thanks for messaging me!</p>;
   }
   return (
+    <div>
     <div className="min-h-screen bg-gray-700 scroll-smooth">
      <div className="pt-24 px-4 md:px-10 pb-8">
         <div className="max-w-3xl mx-auto">
@@ -52,6 +53,7 @@ function ContactForm() {
               </button>
             </form>
    
+      </div>
       </div>
       </div>
       </div>
